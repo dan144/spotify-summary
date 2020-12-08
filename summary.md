78230dbe4194c77c185fb81fedda501404c5c9ee
Daniel Gross <dan.12sq@gmail.com>
2020-12-07 19:39:21 -0500

    add summary.md; clean up output, sequential counter

---
+++ summary.md
@@ -0,0 +1,42 @@
+# Spotify Summary (2020-01-01 - 2020-12-04)
+
+## Total playtime:
+
+87444 min
+
+## Top artists
+
+All Time Low         3040 min
+Carly Rae Jepsen     2872 min
+Bleachers            2154 min
+The Maine            1981 min
+Gary Clark Jr.       1920 min
+Minus the Bear       1891 min
+Poets of the Fall    1874 min
+10 Years             1825 min
+Stick Figure         1612 min
+Gojira               1580 min
+
+## Top songs
+
+Goodmorning                    Bleachers             63 times,   178 min
+Black Butterflies and Déjà Vu  The Maine             58 times,   167 min
+Don't Take The Money           Bleachers             56 times,   179 min
+Hate That You Know Me          Bleachers             50 times,   138 min
+Don't Come Down                The Maine             50 times,   138 min
+Bad Behavior                   The Maine             50 times,   145 min
+Taxi                           The Maine             49 times,   140 min
+Monsters (feat. blackbear)     All Time Low          46 times,   120 min
+Everybody Lost Somebody        Bleachers             46 times,   159 min
+Comeback                       Carly Rae Jepsen      45 times,   157 min
+
+## Artist played most in a row:
+
+Of Mice & Men
+211 times on 2020-08-17
+
+## Song played most in a row:
+
+March Into the Sea
+Modest Mouse
+4 times on 2020-02-25
--- summary.py
+++ summary.py
@@ -16,16 +16,19 @@ artist_count = 1
 last_artist = None
 last_song = None
 
-print('2020 Spotify Summary (1 Jan - 4 Dec)')
+first_date = None
+
 library = {}
 for filename in files:
     with open(filename) as f:
         tracks = json.load(f)
         for track in tracks:
-            last_date = track['endTime'].split()[0]
-            if last_date.startswith('2019'):
+            date = track['endTime'].split()[0]
+            if date.startswith('2019'):
                 # skip anything from 2019
                 continue
+
+            first_date = first_date or date
 
             artist = track['artistName']
             song = track['trackName']
@@ -42,7 +45,7 @@ for filename in files:
             # check is song or artist changed
             if last_artist == artist and last_song == song:
                 song_count += 1
-            else:
+            elif last_artist and last_song:
                 if song_count > song_in_a_row['count']:
                     song_in_a_row = {
                         'artist': last_artist,
@@ -55,7 +58,7 @@ for filename in files:
             # check if artist changed
             if last_artist == artist:
                 artist_count += 1
-            else:
+            elif last_artist:
                 if artist_count > artist_in_a_row['count']:
                     artist_in_a_row = {
                         'artist': last_artist,
@@ -66,7 +69,7 @@ for filename in files:
 
             last_artist = artist
             last_song = song
-            last_date = last_date
+            last_date = date
 
 song_totals = {}
 artist_totals = {}
@@ -76,27 +79,34 @@ for artist, songs in library.items():
         artist_totals[artist] += data['duration']
         song_totals[artist, song] = data
 
-print('Total: {} min'.format(sum(artist_totals.values()) // 1000 // 60))
-
+print('# Spotify Summary ({} - {})'.format(first_date, last_date))
 print()
-print('Top artists')
+print('## Total playtime:')
+print()
+print(sum(artist_totals.values()) // 1000 // 60, 'min')
+print()
+print('## Top artists')
+print()
 for artist, ms in sorted(artist_totals.items(), key=lambda x: x[1], reverse=True)[:10]:
     print('{:<20} {} min'.format(artist, ms // 1000 // 60))
 
 print()
-print('Top songs')
+print('## Top songs')
+print()
 for (artist, song), data in sorted(song_totals.items(), key=lambda x: x[1]['count'], reverse=True)[:10]:
     ms = data['duration']
     count = data['count']
     print('{:<30} {:<20} {:>3} times, {:>5} min'.format(song, artist, count, ms // 1000 // 60))
+print()
+
+print('## Artist played most in a row:')
+print()
+print('{}'.format(artist_in_a_row['artist']))
+print('{} times on {}'.format(artist_in_a_row['count'], artist_in_a_row['date']))
 
 print()
-print('Artist played most in a row:')
-print('    {}'.format(artist_in_a_row['artist']))
-print('    {} times on {}'.format(artist_in_a_row['count'], artist_in_a_row['date']))
-
+print('## Song played most in a row:')
 print()
-print('Song played most in a row:')
-print('    {}'.format(song_in_a_row['song']))
-print('    {}'.format(song_in_a_row['artist']))
-print('    {} times on {}'.format(song_in_a_row['count'], song_in_a_row['date']))
+print('{}'.format(song_in_a_row['song']))
+print('{}'.format(song_in_a_row['artist']))
+print('{} times on {}'.format(song_in_a_row['count'], song_in_a_row['date']))
